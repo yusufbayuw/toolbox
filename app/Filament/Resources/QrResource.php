6edea0d765494ab280f07c874a1bab6a4bff049e
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2025-01-06 13:27:16 +0700

    qrcode

---
+++ app/Filament/Resources/QrResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\QrResource\Pages;
+use App\Filament\Resources\QrResource\RelationManagers;
+use App\Models\Qr;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class QrResource extends Resource
+{
+    protected static ?string $model = Qr::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('content')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('qr_code')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('options')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('content')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('qr_code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListQrs::route('/'),
+            'create' => Pages\CreateQr::route('/create'),
+            'edit' => Pages\EditQr::route('/{record}/edit'),
+        ];
+    }
+}
